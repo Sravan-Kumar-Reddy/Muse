3c81d9d805f4ecea7a1b65c508067be1338bc382
Bontha Sravan Kumar Reddy <bonthasravan@gmail.com>
2019-01-08 19:59:46 +0530

    DB config

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
         ],
 
@@ -72,16 +72,16 @@ return [
 
 
  'redis' => [
- 
+
         'client' => 'predis',
- 
+
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
- 
+
     ],
 
         'sqlsrv' => [
